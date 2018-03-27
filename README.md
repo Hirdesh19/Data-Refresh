4837f5fe936b374918f4b267fd0e3cf2ebb26f93
roberthelmick08 <robert.helmick@toyota.com>
2018-03-27 09:31:04 -0500

    rebase from bitbucket

--- README.md
+++ README.md
@@ -1,30 +1,6 @@
-# CSV-data-refresh
+# Data Refresh
 
 Automated data refresh that parses results from SQL query to CSV, then uploads to FTP server and AWS S3 bucket. Deployed using Serverless to refresh data at user-specified intervals.
-
-## Installation
-
-```
-https://github.com/roberthelmick08/CSV-data-refresh.git
-```
-
-```cd``` into the newly created directory.
-
-```
-npm install
-```
-
-## Deployment
-
-* ```serverless.yml```:
-    * At ```provider.iamRoleStatements.Resource```)(Rows 97-98):
-       * Update with the desired source bucket name
-    * At ```functions.copyFilesToS3.events.s3.bucket``` (Row 134):
-       * Update with the source bucket name
-* ```handler.ts```: 
-    * Update value of ```srcBucket``` global variable (Row 4)
-    * Update FTP ```host```, ```username``` and ```password``` values (Rows 16 - 18)
-* In Git Bash, use command ```serverless deploy```
 
 ## Author
 
--- src/handler.ts
+++ src/handler.ts
@@ -1,55 +1,62 @@
+// Imports
 const AWS = require('aws-sdk-proxy');
 const async = require('async');
 const Client = require('ftp');
 const parser = require('json2csv');
 const sql = require('mssql');
-const util = require('util');
 const fs = require('fs');
-const s3 = new AWS.S3({apiVersion: '2006-03-01', sslEnabled: false});
+
+// CSV Settings
+const csvConfigParams = {
+		 fileName: 'data.csv',
+		 fields: [	'csvColumn1',
+ 					'csvColumn2',
+ 					'csvColumn3',
+ 					'csvColumn4'
+ 				]
+}
+
+// SQL Connection settings
+const sqlConfigParams = {
+		user: 'user',
+		password: 'password',
+		server: 'server',
+		port: 8025,
+		database: 'databaseName'
+}
+
+// FTP Connection settings
 const c = new Client();
-const fileName = 'data.csv';
-const fields = [	'unitId',
-					'dealerCode',
-					'stockNum',
-					'reserveFlag',
-					'comments',
-					'vehicleTradedDate',
-					'daysInStock',
-					'rdrStatus'
-				];
-/*
- * TODO:
- * Code clean-up
- * Obfuscate sensitive info
- * Refactor as node package
- * README
-*/
+const ftpConfigParams = {
+	host: 'host',
+	user: 'user',
+	password: 'password',
+	connTimeout: 5000
+}
+
+// S3 Bucket Connection settings
+const s3 = new AWS.S3({apiVersion: '2006-03-01', sslEnabled: false});
+const s3ConfigParams = {
+		  Bucket: 'bucketName',
+		  Key: csvConfigParams.fileName,
+		  Body: fs.createReadStream(csvConfigParams.fileName)
+}
 
 module.exports.dataRefresh = async function(event, context, callback) {
-	var dealerCd = event;
-	await connectSQL(dealerCd).then(writeToFTP);
+	await connectSQL(event).then(writeToFTP);
  	writeToS3();
 }
 
-async function connectSQL(dealerCd){
+async function connectSQL(query){
 	try {
-		const sqlConfig = {
-				user: 'dduser',
-				password: 'ddu100',
-				server: 'sqld07',
-				port: 8025,
-				database: 'DealerDailyFRDev'
-		};
-		
-		const sqlQuery = "select v.UID as 'unitId', v.dlrcode as 'dealerCode', CASE v.stockno when '' then 'NULL' else v.stockno end as 'stockNum', reserveflag as 'reserveFlag', CASE vehicleComments WHEN '' then 'NULL' else vehicleComments end as 'comments', CONVERT(varchar(24), ReceivedDate, 127) as 'vehicleTradedDate', CASE LastKnownDIS WHEN null then 0 ELSE LastKnownDIS end as 'daysInStock', CASE r.TransactionStatusCode WHEN '50' then 'Pending' WHEN '10' then 'Pending' WHEN '120' then 'Canceled' WHEN '80' then 'Failed' WHEN '250' then 'Pending' WHEN '300' then 'Pending' end as 'rdrStatus' from dd.VehicleInventory v left outer join dd.VehicleRDR r on v.uid = r.uid and v.dlrcode = r.dlrcode"
-						+ " where v.dlrcode in(" + dealerCd + ")";
-
-        let pool = await sql.connect(sqlConfig);
+        let pool = await sql.connect(sqlConfigParams);
+        
         console.log("SQL Connection SUCCESSS");
-        let result = await pool.request().query(sqlQuery);
+        
+        let result = await pool.request().query(query);
         return result;
 	} catch(err){
-		console.log('SQL query error: ' + err);
+		console.err('SQL query error: ' + err);
 		throw err;
 	}
 }
@@ -58,25 +65,21 @@ function writeToFTP(event){
 	// Parse to CSV
 	var csv = parser({data: event.recordset, fields: fields});
 	// Write CSV to local directory
-	fs.writeFile(fileName, csv, function(err){
+	fs.writeFile(csvConfigParams.fileName, csv, function(err){
 		if (err){
+			console.err('FTP Write error' + err);
 			throw err;
 		} else{
 			console.log("Data saved locally");
 			
-			c.connect({
-				host: 'ftp3.test.toyota.com',
-				user: 'ddoa_d',
-				password: 'ZAq12wsx',
-				connTimeout: 5000
-			});
+			c.connect(ftpConfigParams);
 
 			c.on('ready', function(){
 				
 				// TODO: save file to specific directory path
-				c.put(fileName, fileName, function(err){
+				c.put(csvConfigParams.fileName, csvConfigParams.fileName, function(err){
 					if(err){
-						console.log('FTP Copy error: ' + err);
+						console.err('FTP Copy error: ' + err);
 						throw err;
 					} else{
 						console.log("Data uploaded to FTP");
@@ -88,25 +91,17 @@ function writeToFTP(event){
 }
 
 function writeToS3(){
-	
-	console.log("before writeToS3");
-	
-	var params = {
-			  Bucket: 'rti-dev-ftp-s3',
-			  Key: fileName,
-			  Body: fs.createReadStream(fileName)
-	    	}
-	    	
-	s3.putObject(params, function(err, data){
+	s3.putObject(s3ConfigParams, function(err, data){
 		if(err){
-			console.log('Error uploading data to S3 bucket: ' + err);
+			console.err('Error uploading data to S3 bucket: ' + err);
 			throw err;
 		} else{
 			console.log("Data uploaded to S3 bucket: ");
+			
         	// Delete data.csv from local directory
-        	fs.unlink(fileName, function(err){
+        	fs.unlink(csvConfigParams.fileName, function(err){
                 if(err) {
-                	return console.log("Delete error: " + err);
+                	return console.err("Delete error: " + err);
                 }
                 else{
                 	console.log("Data deleted successfully");
